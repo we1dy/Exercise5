8c4f4da68daa41e7f1d2597a49ae6eb0d3e48165
we1dy <ladyarboleda26@gmail.com>
2024-08-28 01:08:48 +0800

    Add project files.

---
+++ Exercise5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Exercise5
+{
+	public static void Main()
+	{
+
+		double r, per_cir;
+		double PI = 3.14;
+		Console.WriteLine("Input the radius of the circle : ");
+		r = Convert.ToDouble(Console.ReadLine());
+		per_cir = 2 * PI * r;
+		Console.WriteLine("Perimeter of Circle : {0}", per_cir);
+		Console.Read();
+	}
+}
